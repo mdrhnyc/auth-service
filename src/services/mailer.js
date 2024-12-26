85b26dbf10fc5b2a992154078159cd5871f86f99
Md Raqueebul Hussain <raqueeb@mds-macbook-pro.mynetworksettings.com>
2024-12-26 01:35:02 -0500

    some mail config changes

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -3,10 +3,14 @@ const nodemailer = require('nodemailer');
 let transporter;
 
 function createTransporter() {
+    //console.log(process.env.GMAIL_USER);
+    //console.log(process.env.GMAIL_PASSWORD);
     // Check if the transporter is already created; if not, create it
     if (!transporter) {
         transporter = nodemailer.createTransport({
-            service: 'gmail',  // Use Gmail service for sending emails
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.GMAIL_USER,     // Your Gmail email address (set in .env)
                 pass: process.env.GMAIL_PASSWORD, // Your Gmail app password (set in .env)
